3a51e89123f9170e066d04ab4a0bf5095f1e4cd1
omdubey <omdubey1021@gmail.com>
2024-08-28 11:38:43 +0530

    Added mailing service successfully

--- src/main/java/net/engineeringdigest/journalApp/service/EmailService.java
+++ src/main/java/net/engineeringdigest/journalApp/service/EmailService.java
@@ -1,4 +1,34 @@
 package net.engineeringdigest.journalApp.service;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.stereotype.Service;
+
+import java.util.Properties;
+
+@Service
+@Slf4j
 public class EmailService {
+
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    public void sendMail(String to, String subject, String body) {
+        try {
+
+            SimpleMailMessage mail = new SimpleMailMessage();
+            mail.setTo(to);
+            mail.setSubject(subject);
+            mail.setText(body);
+            javaMailSender.send(mail);
+
+        } catch (Exception e) {
+            log.error("Something went wrong while sending mail", e);
+        }
+    }
+
 }
--- src/test/java/net/engineeringdigest/journalApp/service/EmailServiceTests.java
+++ src/test/java/net/engineeringdigest/journalApp/service/EmailServiceTests.java
@@ -1,4 +1,20 @@
 package net.engineeringdigest.journalApp.service;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
 public class EmailServiceTests {
+
+    @Autowired
+    EmailService emailService;
+
+    @Test
+    public void sendMailTest() {
+        String body = "Java Mail Sender Implemented successfully";
+        String subject = "Finally its working fine";
+        String to = "omdubey1021@gmail.com";
+        emailService.sendMail(to, subject, body);
+    }
 }
